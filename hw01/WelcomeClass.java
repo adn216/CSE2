991f9d1ae126d0700a1111dbe28c2c23df11b794
Aimee Denyse Niwenshuti <adn216@lehigh.edu>
2014-09-01 17:16:59 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////////////////
+//Aimee Denyse Niwenshuti
+//Welcome Class Java Program
+//September 2nd, 2014
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//   run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+
+// add main method
+  public static void main(String[ ] args) {
+     
+//  print the statement with the box around it
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-A--D--N--2--1--6-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("I started learning Swahili this summer");
+    System.out.println("test");
+    
+    //test/testtest
+  }
+}
