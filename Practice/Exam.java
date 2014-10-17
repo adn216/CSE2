43c648d620c8e6b33af4e39a92eeabd812fe17dd
Aimee Denyse Niwenshuti <adn216@lehigh.edu>
2014-10-17 13:28:44 +0000

    updated Enigma0

--- Practice/Exam.java
+++ Practice/Exam.java
@@ -1,63 +1,36 @@
-// Aimee Denyse Niwenshuti
-// Exam prep
-// Sept 24th
+/*Sample of expected output:
+ *   Enter an int- 9
+ *   You entered 9
+ *   sum is 25
+ *   Again, you entered 9
+ */
+
 import java.util.Scanner;
 public class Exam{
-	public static void main(String[] arg){
-    Scanner scan=new Scanner(System.in);
-    int n=0;
-
+  public static void main(String arg []){
     System.out.print("Enter an int- ");
-    while(!scan.hasNextInt()){
-    	scan.next(); //get rid of the junk entered by user
-    	System.out.print("You did not enter an int; try again- ");
+    Scanner scan=new Scanner(System.in);
+    if(scan.hasNextInt()){
+      int n=scan.nextInt();
+      System.out.println("You entered "+n);
     }
-    //COMMENT OUT THE ABOVE 5 LINES AND REPLACE THE CODE WITH A
-    //DO-WHILE LOOP
-    	n=scan.nextInt();
-
-    for(int j=0;j<n && j<40;j++){
-    	for(int k=0;k<j+1;k++){
-   	 System.out.print('*');
-    	}
-    	System.out.println();
+    else{
+      int n=4;
+      System.out.println("You entered "+n);
     }
-    //COMMENT OUT THE ABOVE SIX LINES OF CODE AND REPLACE WITH
-    //WHILE STATEMENTS
- 	 
-    int k=4;
-
-    do{
-    	System.out.println("k="+k);
-    	k++;
+    
+    int k=4,p=6,q=7,r=8,n;
+    switch(k+p+q+r){
+      case 24: 
+      case 25: System.out.println("sum is 25");
+      default:
+        System.out.println("To repeat, you entered "+n);
     }
-     	while(k<4);
-    	//COMMENT OUT THE ABOVE FIVE LINES AND REPLACE THE CODE
-    //WITH A WHILE LOOP
-
-    	int count=0;
-    	while(true){
-    	switch(n){
-    	default: System.out.println(n + " is > 5 or <1");
-   	 break;
-    	case 1:
-    	case 2: System.out.print("Case 2 ");
-   	 continue;
-    	case 3: break;
-    	case 4: System.out.println("Case 4");
-    	case 5: System.out.println("Case 5");
-   	 break;
-    	}
-    	count++;
-    	if(count>10)
-   	 break;
-    }
-    //COMMENT OUT THE ABOVE 16 LINES AND REWRITE THE CODE USING
-    // ONLY FOR LOOPS AND IF STATEMENS. BEFORE DOING SO, TRY
-    //RUNNING THE CODE WITH VARIOUS INPUTTED VALUES, E.G.,
-    //   -5, 1, 20, 5, ETC.
-    //IF YOU GET AN INFINITE LOOP, YOU CAN STOP IT BY
-    //SIMULTANEOUSLY PRESSING THE CTRL KEY AND THE C KEY
-	}
+  }
 }
+
+/* Error report:
+ *   Expand this comment:
+ *   Explain the error(s) that occurred here, and then fix them
+ */
  
--- lab06/Enigma0.java
+++ lab06/Enigma0.java
@@ -31,8 +31,32 @@ public class Enigma0{
     }
   }
 }
-
+/*import java.util.Scanner;
+public class Exam{
+  public static void main(String arg []){
+    System.out.print("Enter an int- ");
+    Scanner scan=new Scanner(System.in);
+    if(scan.hasNextInt()){
+      int n=scan.nextInt();
+      System.out.println("You entered "+n);
+    }
+    else{
+      int n=4;
+      System.out.println("You entered "+n);
+    }
+    
+    int k=4,p=6,q=7,r=8,n;
+    switch(k+p+q+r){
+      case 24: 
+      case 25: System.out.println("sum is 25");
+      default:
+        System.out.println("To repeat, you entered "+n);
+    }
+  }
+}*/
 /* Error report:
- *   Expand this comment:
- *   Explain the error(s) that occurred here, and then fix them
+ *  int n was not initialized outside if
+ *  To correct it, I initialized n before starting the if statement
+ *  So n could be used in other statement without having to initialize so
+ *  many times
  */
