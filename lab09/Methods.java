38a40992dad9130a39de3e9a66b57793aab14999
Aimee Denyse Niwenshuti <adn216@lehigh.edu>
2014-10-24 14:05:37 +0000

    updated Methods

---
+++ lab09/Methods.java
@@ -0,0 +1,77 @@
+// Aimee Denyse Niwenshuti
+// CSE 2 lab09
+// Methods
+// Oct 24th, 2014
+
+import java.util.Scanner;
+public class Methods{
+  public static void main(String [] arg){
+	Scanner scan=new Scanner(System.in);
+	int a,b,c;
+	System.out.println("Enter three ints");
+	
+	a=getInt(scan);
+	b=getInt(scan);
+	c=getInt(scan);
+
+	System.out.println("The larger of "+a+" and "+b+" is "+larger(a,b));
+	System.out.println("The larger of "+a+", "+b+", and "+c+
+                   	" is "+larger(a,larger(b,c)));
+	
+	System.out.println("It is "+ascending(a,b,c)+" that "+a+", "+b+
+                   	", and "+c+" are in ascending order");
+	
+  }
+
+public static int getInt(Scanner scan) {
+    
+    while (!scan.hasNextInt()){
+        System.out.println("You did not enter an int, try again");
+        
+    }
+    int e=scan.nextInt();
+    return e;
+}
+
+public static int larger(int a, int b){
+    int d ;
+    if (a>b){
+        d=a;
+    }
+    else{
+        d=b;
+    }
+    return d;
+}
+
+public static boolean ascending(int a, int b, int c){
+    
+    if (a>b && b>c){
+        return true;
+    }
+    
+    else{
+        return false;
+    }
+    
+}
+    
+}
+/*******SAMPLE RUNS:
+Enter three ints
+Enter an int- r
+You did not enter an int; try again- 3
+Enter an int- 8
+Enter an int- 9
+The larger of 3 and 8 is 8
+The larger of 3, 8, and 9 is 9
+It is true that 3, 8, and 9 are in ascending order
+
+Enter three ints
+Enter an int- 4
+Enter an int- 3
+Enter an int- 7
+The larger of 4 and 3 is 4
+The larger of 4, 3, and 7 is 7
+It is false that 4, 3, and 7 are in ascending order
+*/
