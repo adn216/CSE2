0336fb0172116a0024d93994535fdd6bcc79bf5d
Aimee Denyse Niwenshuti <adn216@lehigh.edu>
2014-11-04 21:52:10 +0000

    updated BlockedAgain

--- Practice/Exam.java
+++ Practice/Exam.java
@@ -4,57 +4,24 @@
  * the program to crash
  */
 
-import java.util.Scanner;
+//import java.util.Scanner;
 public class Exam{
   public static void main(String [] arg){
-    int n=40,k=60;
-    String out="";
-    switch(k%14){
-      case 12: 
-      case 13: out+=13;
-         break;
-      default: out+=40;
-          n/=3;
-          k-=7;
-    }
-    if(n*k%12< 12){
-      n-=20;
-      out+=n;
-    }
-    if(n*n>k){
-      n=42;
-      out+=n+k;
-    }
-    else {
-      n=45;
-      out+=n+k;
-    }
-    switch(n+k){
-      case 114: 
-        n-=11;
-        k-=3;
-        break;
-      case 97:
-        n-=14;
-        k-=2;
-        break;
-      case 98:
-        n/=5;
-        k/=9;
-      default:
-        n-=3;
-        k-=5;
-    }
-     out+=1/n + 1/k;   
-    System.out.println(out);
+  
+  //Scanner input= new Scanner(System.in);
+  int i=1;
+  while(i<=6){
+    method1(i, 2);
+    i++;
+  }
+  
+ }
+ 
+public static void method1(int i, int num){
+  for(int j=1; j<=i; j++){
+    System.out.println(num +" ");
+    num *=2;
   }
 }
 
-/*
- * Error report:
- * 
- * 
- * 
- * 
- */
- 
+}
